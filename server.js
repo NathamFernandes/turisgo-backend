83e1e102801e4b68a658127b0bc898c50e99e879
nathamfernandes <natham.fr@gmail.com>
2026-06-21 20:38:25 -0300

    fix: utilização de cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // server.js
 import express from 'express';
+import cors from 'cors';
 import { listarLocais, criarLocal, listarRoteiros, criarRoteiro } from './funcoes.js';
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
